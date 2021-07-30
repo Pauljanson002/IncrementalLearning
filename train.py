4a77d1f08d1d78a66f84088b7d61584e9f66d584
Paul Janson <pauljanson002@gmail.com>
2021-07-30 14:19:00 +0530

    Added directory

--- train.py
+++ train.py
@@ -2,7 +2,12 @@ import wandb
 
 from IncrementalTrainingApproach.iCaRL import iCaRLmodel
 from models.ResNet import resnet18_cbam
+import numpy as np
 import torch
+torch.backends.cudnn.benchmark = True
+np.random.seed(1)
+torch.manual_seed(1)
+torch.cuda.manual_seed(1)
 
 numclass = 10
 feature_extractor = resnet18_cbam()
@@ -10,7 +15,7 @@ img_size = 32
 batch_size = 128
 task_size = 10
 memory_size = 2000
-epochs = 10
+epochs = 100
 learning_rate = 2.0
 
 args = dict(
@@ -29,7 +34,7 @@ model = iCaRLmodel(numclass, feature_extractor, batch_size, task_size, memory_si
 for i in range(10):
     args["task_id"] = i
     run = wandb.init(
-        project="icarl",
+        project="icarl1",
         reinit=True,
         config=args,
     )
